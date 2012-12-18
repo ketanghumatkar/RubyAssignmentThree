db6a16756092954438317158dd659a61627931b1
ketan-webonise <ketan@weboniselab.com>
2012-12-18 19:13:31 +0530

    ma_first_project added

---
+++ ma_first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaFirstApp::Application.config.secret_token = '988de4c2c2f6982ceb3e479de7a65b72fd2a6965ff3cb0e8af4377e6fc83c9872aba84fdd88750b6ca3444b505f64557e275e3f2d449b1adba22f941b3f64794'
